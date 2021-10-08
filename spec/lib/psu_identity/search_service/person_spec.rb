2b1f8532098428531f62b5acffe4ffec1b41d418
Adam Wead <awead@users.noreply.github.com>
2021-10-08 14:01:14 -0400

    Allow boolean values for active status in Person

--- spec/lib/psu_identity/search_service/person_spec.rb
+++ spec/lib/psu_identity/search_service/person_spec.rb
@@ -88,14 +88,26 @@ RSpec.describe PsuIdentity::SearchService::Person do
   end
 
   describe 'active?' do
-    context 'when the person is active' do
+    context 'when set to "true"' do
       subject { described_class.new('active' => 'true') }
 
       it { is_expected.to be_active }
     end
 
-    context 'when the person is NOT active' do
+    context 'when set to a boolean true' do
+      subject { described_class.new('active' => true) }
+
+      it { is_expected.to be_active }
+    end
+
+    context 'when set to "false"' do
       subject { described_class.new('active' => 'false') }
+
+      it { is_expected.not_to be_active }
+    end
+
+    context 'when set to a boolean false' do
+      subject { described_class.new('active' => false) }
 
       it { is_expected.not_to be_active }
     end
